db46d038c2e44e86b79c818ac2ab7043cd311cd1
spydworld3123 <ankit.224050@sggscc.ac.in>
2025-04-02 01:21:41 +0530

    object destrcuture

---
+++ Objectdestructure.js
@@ -0,0 +1,18 @@
+const newobject = {
+    cname: "sjadas",
+    price : 32432,
+    cisd: "ashdsabdasd"
+}
+const {cname}= newobject
+console.log(cname);
+
+
+const {cname: newvar}= newobject
+console.log(newvar);
+
+[
+    {},
+    {},
+    {},
+]
+//json
